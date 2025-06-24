72cf6ae6d1ccfe380814507eaa8e1bfef229bb4f
Abd44Arfat <abdoarfat924@gmail.com>
2025-06-24 12:23:13 +0300

    first commit

---
+++ movieapp/Views/CustomTitleAndSeeAllHeader.swift
@@ -0,0 +1,8 @@
+//
+//  CustomTitleAndSeeAllHeader.swift
+//  movieapp
+//
+//  Created by ABDO on 24/06/2025.
+//
+
+import Foundation
---
+++ movieapp/Views/HomeView.swift
@@ -0,0 +1,8 @@
+//
+//  HomeView.swift
+//  movieapp
+//
+//  Created by ABDO on 24/06/2025.
+//
+
+import Foundation
